0c963e4a8c70661fad34c34e2a1cc072614db27e
Kleber Araujo <klbr16@outlook.com>
2025-05-11 23:34:36 -0300

    servidor

--- server/servidor.py
+++ server/servidor.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
 import socket
 import threading
 from dataBase.banco import processar_requisicao, inicializar_banco
