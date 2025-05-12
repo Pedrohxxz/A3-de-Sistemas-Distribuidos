d07e090a29acf4984ac81d5d06103c3c6a6896d8
Kleber Araujo <klbr16@outlook.com>
2025-05-11 23:30:19 -0300

    Adicionando a função de cancelar a reserva.

--- dataBase/banco.py
+++ dataBase/banco.py
@@ -24,8 +24,12 @@ def processar_requisicao(mensagem):
 
     if comando == "ATENDENTE_CRIAR":
         return criar_reserva(*partes[1:])
+    elif comando == "ATENDENTE_CANCELAR":
+        return cancelar_reserva(*partes[1:])
     elif comando == "GARCOM_CONFIRMAR":
-        return confirmar_reserva(int(partes[1]))
+        return confirmar_reserva(
+            (int(partes[1])), (int(partes[2])), (partes[3]), (partes[4])
+        )
     elif comando == "GERENTE_RELATORIO_MESA":
         return relatorio_por_mesa(int(partes[1]))
     else:
@@ -34,36 +38,67 @@ def processar_requisicao(mensagem):
 def criar_reserva(data, hora, numero_mesa, quantidade, nome):
     conn = sqlite3.connect("banco.sqlite")
     cursor = conn.cursor()
-    
+
     # Verifica se já existe uma reserva com essas informações aí data/hora/mesa
     cursor.execute("SELECT * FROM reservas WHERE data = ? AND hora = ? AND numero_mesa = ? AND status = 'reservado'",
                 (data, hora, numero_mesa))
     if cursor.fetchone():
         conn.close()
         return "Erro: mesa já reservada nesse horário."
-    
+
     cursor.execute('''
         INSERT INTO reservas (data, hora, numero_mesa, quantidade_pessoas, nome_responsavel, status)
         VALUES (?, ?, ?, ?, ?, 'reservado')
     ''', (data, hora, numero_mesa, quantidade, nome))
-    
+
     conn.commit()
     conn.close()
     return "Reserva criada com sucesso."
 
-def confirmar_reserva(numero_mesa):
+
+def cancelar_reserva(data, hora, numero_mesa, nome):
     conn = sqlite3.connect("banco.sqlite")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM reservas WHERE numero_mesa = ? AND status = 'reservado'", (numero_mesa,))
+
+    cursor.execute(
+        """
+        SELECT * FROM reservas 
+        WHERE data = ? AND hora = ? AND numero_mesa = ? AND nome_responsavel = ? AND status = 'reservado'
+    """,
+        (data, hora, numero_mesa, nome),
+    )
+    reserva = cursor.fetchone()
+
+    if not reserva:
+        conn.close()
+        return "Erro: reserva não encontrada para cancelamento."
+
+    cursor.execute("DELETE FROM reservas WHERE id = ?", (reserva[0],))
+    conn.commit()
+    conn.close()
+    return "Reserva cancelada com sucesso."
+
+
+def confirmar_reserva(garcom_id, numero_mesa, data, hora):
+    conn = sqlite3.connect("banco.sqlite")
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT * FROM reservas WHERE numero_mesa = ?  AND data = ? AND hora = ? AND status = 'reservado'",
+        (numero_mesa, data, hora),
+    )
     reserva = cursor.fetchone()
     if not reserva:
         conn.close()
-        return "Erro: nenhuma reserva ativa encontrada para essa mesa."
-    
-    cursor.execute("UPDATE reservas SET status = 'confirmado' WHERE id = ?", (reserva[0],))
+        return "Erro: Nenhuma reserva ativa encontrada para essa mesa."
+
+    cursor.execute(
+        "UPDATE reservas SET status = 'confirmado', garcom_id = ?  WHERE id = ?",
+        (garcom_id, reserva[0]),
+    )
     conn.commit()
     conn.close()
     return "Reserva confirmada com sucesso."
+
 
 def relatorio_por_mesa(numero_mesa):
     conn = sqlite3.connect("banco.sqlite")
